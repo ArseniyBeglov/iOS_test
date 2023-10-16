8f4557e8b295ed2b85367130769d3cd9cf9373fb
ArseniyBeglov <HUDAIDOTT@yandex.ru>
2023-10-16 19:42:34 +0300

    added url session

--- iOS_test/ViewController.swift
+++ iOS_test/ViewController.swift
@@ -7,14 +7,27 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
-        // Do any additional setup after loading the view.
+        let url: URL = URL(string: "https://api.punkapi.com/v2/beers")!
+        URLSession.shared.dataTask(with: url, completionHandler: {data, response, error in
+            guard let data = data,
+                  let response = response,
+                  error == nil
+            else{
+                return
+            }
+//            let decoder = JSONDecoder()
+//            let decodedData = try decoder.decode(MyData.self, from: data)
+            let str = String(data:data, encoding: .utf8)
+            print("\(str ?? "")")
+        }).resume()
+        
     }
-
-
 }
 
